325cea5182416db8b556557adc4bee557f84d062
Lukas Neururer <neururer.luk@gmail.com>
2026-06-29 17:40:39 +0200

    implemented filter functionality for image gallery

---
+++ src/app/image.interface.ts
@@ -0,0 +1,6 @@
+export interface Image {
+  id: number;
+  title: string;
+  category: string;
+  imageUrl: string;
+}
---
+++ src/app/images.ts
@@ -0,0 +1,22 @@
+import { Image } from './image.interface';
+
+export const IMAGES: Image[] = [
+  {
+    id: 1,
+    title: 'Mountains',
+    category: 'landscape',
+    imageUrl: 'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=500&h=400&fit=crop',
+  },
+  {
+    id: 2,
+    title: 'Dude',
+    category: 'portrait',
+    imageUrl: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=500&h=400&fit=crop',
+  },
+  {
+    id: 4,
+    title: 'Model Portrait',
+    category: 'portrait',
+    imageUrl: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=500&h=400&fit=crop',
+  },
+];
