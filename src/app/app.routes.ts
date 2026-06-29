cb57fa550c6f1ecd0a8d443410fda8df6669859c
Lukas Neururer <neururer.luk@gmail.com>
2026-06-29 11:44:13 +0200

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Portfolio } from './portfolio/portfolio';
+import { Details } from './details/details';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'portfolio', component: Portfolio },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+  { path: 'details', component: Details },
+];
