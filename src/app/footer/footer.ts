1b05956f7fd14c92913492ac12d738cdd35fdd0d
Havokitten <havokitten@gmail.com>
2026-07-01 22:14:41 +0200

    improved footer design

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {
-  refreshPage(event: Event): void {
+export class Footer { // edit to change scroll to top functionality
+  handleSocialClick(event: Event): void {
     event.preventDefault();
-    window.location.reload();
   }
+
   scrollToTop(event: Event): void {
     event.preventDefault();
+    const topElement = document.getElementById('top');
+
+    if (topElement) {
+      topElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      return;
+    }
+
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
