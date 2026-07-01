567491e2dd340cd72880ab73a3fcb38b021daee7
Havokitten <havokitten@gmail.com>
2026-07-01 22:21:02 +0200

    simplified navbar brand design + added transition from transparent to solid navbar on scroll + fixed navbar not sticking + responsiveness tweaks

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +7,21 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  isScrolled = false;
+
+  constructor(private router: Router) {}
+
+  get useSolidNavbar(): boolean {
+    return this.isScrolled || this.router.url.startsWith('/details'); // portfolio details page doesnt have a hero img so a transparent navbar needs to behave differently on that page
+  }
+
+  get isPortfolioSection(): boolean {
+    return this.router.url.startsWith('/portfolio') || this.router.url.startsWith('/details');
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    this.isScrolled = window.scrollY > 24;
+  }
+}
